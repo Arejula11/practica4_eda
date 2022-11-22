422cd8e95edee5e9c3375ea1f116ab2bb4a53dbf
pabloangusto <pabloangusto@gmail.com>
2022-11-22 19:51:51 +0100

    siguiente separado
    Co-authored-by: Miguel Aréjula Aisa <Arejula11@users.noreply.github.com>

--- multi.hpp
+++ multi.hpp
@@ -72,7 +72,7 @@ struct Multi{
         struct Celda{
             Celda* siguiente;
             Elemento clave;
-            int valor;
+            int valor; //multiplicidad
         };
         int tamanyo;
         Celda* primero;
@@ -85,27 +85,94 @@ void vacio(Multi<Elemento>& m){
     m.primero==nullptr;
     m.tamanyo = 0;
 }
+
 template <typename Elemento>
 void anyadir(Multi<Elemento>& m, const Elemento& e){
-    Celda aux;
-    if(m.tamanyo==0){
-        
-    }else if(){
-
-    }else{
-    
+    if(m.primero==nullptr){ //vacia
+        m.primero = new typename Multi<Elemento>::Celda;
+        m.primero->clave = e;
+        m.primero->valor = 1;
+        m.primero->siguiente=nullptr;
+    }else{ //no vacia 
+        typename Multi<Elemento>::Celda* aux;
+        aux = nullptr;
+        if(e<m.primero->clave){ //nuevo menor que el primero
+        aux=m.primero;
+        m.primero = new typename Multi<Elemento>::Celda;
+        m.primero->clave=e;
+        m.primero->valor=1;
+        m.primero->siguiente=aux;
+        m.tamanyo++;
+        }else{
+            if(e==m.primero->clave){
+                m.primero->valor++;
+            }else{ // buscar punto de insercción
+                aux=m.primero;
+                while(aux->siguiente && (aux->siguiente)->clave<e){
+                    aux = aux->siguiente;
+                }
+                if(aux->siguiente!=nullptr && e==(aux->siguiente)->clave){
+                    (aux->siguiente)->valor++;
+                }else{
+                    typename Multi<Elemento>::Celda* nuevo;
+                    nuevo->clave=e;
+                    nuevo->valor++;
+                    nuevo->siguiente= aux->siguiente;
+                    aux->siguiente = nuevo;
+                    m.tamanyo++;
+                }
+            }
+        }
     }
-    m.tamanyo++;
+    
 }
 
 
 template <typename Elemento>
-void quitar(Multi<Elemento>& m, const Elemento& e);
+void quitar(Multi<Elemento>& m, const Elemento& e){
+    typename Multi<Elemento>::Celda* aux1;
+    typename Multi<Elemento>::Celda* aux2;
+    if(m.primero!=nullptr){
+        if(e>=m.primero->clave){
+            if(m.primero->clave==e){
+                aux1=m.primero;
+                m.primero=m.primero->siguiente;
+                delete aux1;
+                m.tamanyo--;
+            }else{
+                bool parar=false;
+                aux1=m.primero->siguiente;
+                aux2=m.primero;
+                while(aux1 == nullptr && !parar){
+                    if(e<aux1->clave){
+                        parar=true;
+                    }else if(e=aux1->clave){
+                        aux2->siguiente=aux1.siguiente;
+                        delete(aux1);
+                        parar = true;
+                        m.tamanyo--;
+                    }else{
+                        aux2=aux1;
+                        aux1=aux1->siguiente;
+                    }
+                }
+            }
+        }
+    }
+}
 
 
 template <typename Elemento>
 int multiplicidad(Multi<Elemento>& m, const Elemento& e){
-    
+    typename Multi<Elemento>::Celda* aux;
+    while(aux != nullptr && aux->clave < e){
+        aux = aux->siguietne;
+    }
+    if(aux != nullptr){
+        if(aux->clave = e){
+            return (aux->valor);
+        }
+    }
 }
 
 
@@ -122,19 +189,37 @@ void iniciarIterdaor(Multi<Elemento>& m ){
 
 
 template <typename Elemento>
-bool existeSiguiente(Multi<Elemento>& m );
+bool existeSiguiente(Multi<Elemento>& m ){
+    return m.iter != nullptr;
+}
+
+
 
 
 template <typename Elemento>
-void siguienteElem(Multi<Elemento>& m );
+void siguienteElem(Multi<Elemento>& m, Elemento& e){
+    if(existeSiguiente(m)){
+        e = m.iter->clave;
+    }
+}
 
 
 template <typename Elemento>
-void siguienteNumRep(Multi<Elemento>& m );
+void siguienteNumRep(Multi<Elemento>& m, int& num ){
+    if(existeSiguiente(m)){
+        num =  m.iter->valor;
+    }
+}
 
 
 template <typename Elemento>
-void avanza(Multi<Elemento>& m );
+void avanza(Multi<Elemento>& m ){
+    if(existeSiguiente(m)){
+       m.iter = m.iter->siguiente;
+    }
+
+
+}
 
 
 
