d27cb6d3fc4847d28667499bd787bf61839eb759
Miguel Aréjula Aisa <arejula10@gmail.com>
2022-11-22 23:09:34 +0100

    coms

--- main.cpp
+++ main.cpp
@@ -1,101 +1,94 @@
 /* Pablo Ernesto Angusto Delgado 842255 y  Miguel Aréjula Aisa 850068
-*/
+ */
 
-#include <iostream>
-#include <fstream>
 #include "multi.hpp"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
-/* Programa de prueba del módulo Multi.hpp que crea un multiconjunto vacío de strings y luego lee un fichero "entrada.txt" 
- * con operaciones a realizar con ese multiconjunto, indicadas con la siguiente sintaxis, y escribe en un fichero "salida.txt" 
- * lo indicado según cada opción:
- * A -> añade un ejemplar del string incluido en la siguiente línea y escribe en salida "AÑADIDO string"
- * Q -> quita (si existe) un ejemplar del string incluido en la siguiente línea y escribe en salida "QUITADO string"
- *      o "NO QUITADO string", según corresponda
- * M -> escribe en salida la multiplicidad (M) del string incluido en la siguiente línea (0, si no existe) con la
- *      sintaxis: MULTIPLICIDAD DE string = M
- * L -> escribe en salida todos los elementos distintos del multiconjunto (sea C el cardinal), uno por línea y con formato:
- *      "string M" (donde M es la multiplicidad de ese string),   
- *      precedidos por una primera línea con "--- CARDINAL = C", y finalizando con una línea con "---". 
+/* Programa de prueba del módulo Multi.hpp que crea un multiconjunto vacío de
+ * strings y luego lee un fichero "entrada.txt" con operaciones a realizar con
+ * ese multiconjunto, indicadas con la siguiente sintaxis, y escribe en un
+ * fichero "salida.txt" lo indicado según cada opción: A -> añade un ejemplar
+ * del string incluido en la siguiente línea y escribe en salida "AÑADIDO
+ * string" Q -> quita (si existe) un ejemplar del string incluido en la
+ * siguiente línea y escribe en salida "QUITADO string" o "NO QUITADO string",
+ * según corresponda M -> escribe en salida la multiplicidad (M) del string
+ * incluido en la siguiente línea (0, si no existe) con la sintaxis:
+ * MULTIPLICIDAD DE string = M L -> escribe en salida todos los elementos
+ * distintos del multiconjunto (sea C el cardinal), uno por línea y con formato:
+ *      "string M" (donde M es la multiplicidad de ese string),
+ *      precedidos por una primera línea con "--- CARDINAL = C", y finalizando
+ * con una línea con "---".
  * */
 
 int main() {
 
-    Multi<string> multiconjcad;
-    
-	vacio(multiconjcad);
-    
-    ifstream inf;
-	inf.open("entrada.txt");
-	ofstream outf;
-	outf.open("salida2.txt");
-    
-    string instruccion;
-	string salto;
-    string separador = "---";
-    
-    while (inf >> instruccion) {
-        getline(inf, salto); //leer salto de linea
-        if (instruccion == "A") {
-            string elemento;
-            getline(inf,elemento);
-			
-			anyadir(multiconjcad,elemento);
-			
-            outf << "AÑADIDO " << elemento << endl;
-        } else if (instruccion == "Q") {
-            string elemento;
-            getline(inf,elemento);
-            
-			int num = cardinal(multiconjcad);
-			// AQUÍ FALTA COMPLETAR CÓDIGO (hágase lo más eficiente posible, teniendo
-			// en cuenta que la operación cardinal tiene coste constante)
-			
-            quitar(multiconjcad,elemento);
-                
-            
-                
-            
-            
-            if (num != cardinal(multiconjcad)) {
-                outf << "QUITADO " << elemento << endl;
-            } else {
-                outf << "NO QUITADO " << elemento << endl;
-            }
-        } else if (instruccion == "M") {
-            string elemento;
-            getline(inf,elemento);
-			int multipli;
-			
-			// AQUÍ FALTA COMPLETAR CÓDIGO
-            multipli=multiplicidad(multiconjcad, elemento);
-			
-            outf << "MULTIPLICIDAD DE " << elemento << " = " << multipli << endl;
-			
-        } else if (instruccion == "L") {
-			int elcardinal;
-            iniciarIterador(multiconjcad);
-			
-			// AQUÍ FALTA COMPLETAR CÓDIGO
-			elcardinal=cardinal(multiconjcad);
-            
-            outf << separador << " CARDINAL = " << elcardinal << endl;
-			
-            // AQUÍ FALTA COMPLETAR CÓDIGO
-			int num; 
-            string e;
-            while (existeSiguiente(multiconjcad)){
-                if(siguiente(multiconjcad,e, num)){
-                    outf<<e<<" "<<num<<endl;
-                }
-            }
-            
-            outf << separador << endl;
-        }
+  Multi<string> multiconjcad;
+
+  vacio(multiconjcad);
+
+  ifstream inf;
+  inf.open("entrada.txt");
+  ofstream outf;
+  outf.open("salida2.txt");
+
+  string instruccion;
+  string salto;
+  string separador = "---";
+
+  while (inf >> instruccion) {
+    getline(inf, salto); // leer salto de linea
+    if (instruccion == "A") {
+      string elemento;
+      getline(inf, elemento);
+
+      anyadir(multiconjcad, elemento);
+
+      outf << "AÑADIDO " << elemento << endl;
+    } else if (instruccion == "Q") {
+      string elemento;
+      getline(inf, elemento);
+
+      int num = cardinal(multiconjcad);
+
+      quitar(multiconjcad, elemento);
+      if (num != cardinal(multiconjcad)) {
+        outf << "QUITADO " << elemento << endl;
+      } else {
+        outf << "NO QUITADO " << elemento << endl;
+      }
+    } else if (instruccion == "M") {
+      string elemento;
+      getline(inf, elemento);
+      int multipli;
+
+      multipli = multiplicidad(multiconjcad, elemento);
+
+      outf << "MULTIPLICIDAD DE " << elemento << " = " << multipli << endl;
+
+    } else if (instruccion == "L") {
+      int elcardinal;
+      iniciarIterador(multiconjcad);
+
+      elcardinal = cardinal(multiconjcad);
+
+      outf << separador << " CARDINAL = " << elcardinal << endl;
+
+      int num;
+      string e;
+      while (existeSiguiente(multiconjcad)) {
+        siguiente(multiconjcad, e, num); 
+          outf << e << " " << num << endl;
+        
+      }
+
+      outf << separador << endl;
     }
-    
-    outf.close();
-	inf.close();
-    return 0;
+  }
+
+  outf.close();
+  inf.close();
+  return 0;
 }
--- multi.hpp
+++ multi.hpp
@@ -1,6 +1,5 @@
 /*Pablo Ernesto Angusto Delgado 842255 y  Miguel Aréjula Aisa 850068
-*/
-
+ */
 
 #ifndef MULTI_HPP
 #define MULTI_HPP
@@ -9,212 +8,234 @@ using namespace std;
 
 // PREDECLARACIÓN DEL TAD GENÉRICO multicomjuntos
 
+// Sus valores multiconjuntos de elementos, es decir, colecciones de elementos
+// en las que puede haber ejemplares repetidos
+template <typename Elemento> struct Multi;
+
+// Devuelve un multiconjunto vacío, sin elementos
+template <typename Elemento> void vacio(Multi<Elemento> &m);
+
+// Devuelve el multiconjunto igual al resultante de añadir un ejemplar del
+// elemento "e" a "m"
 template <typename Elemento>
-struct Multi;
+void anyadir(Multi<Elemento> &m, const Elemento &e);
 
-//
+// Devuelve el multiconjunto igual al resultante de extraer un ejemplar del
+// elemento "e" a "m"; si no hay ningún elemento de "e" en "m", devuelve el
+// multiconjunto igual a m
+template <typename Elemento> void quitar(Multi<Elemento> &m, const Elemento &e);
+
+// Devuelve el número de ejemplares del elemento "e" en "m" (0 si no hay
+// ninguno)
 template <typename Elemento>
-void vacio( Multi<Elemento>& m);
+int multiplicidad(Multi<Elemento> &m, const Elemento &e);
 
-//
+// Devuelve el número total de elementos en "m", contando los repetidos
+template <typename Elemento> int cardinal(Multi<Elemento> &m);
+
+// Prepara el iterador para que el siguiente elemento a visitar sea el menor de
+// todo el multiconjunto "m" (según la operación menor del parámetro formal
+// elemento), si existe
+template <typename Elemento> void iniciarIterador(Multi<Elemento> &m);
+
+// Devuelve falso si ya se han visitado todos los elementos de "m", devuelve
+// verdad en caso contrario
+template <typename Elemento> bool existeSiguiente(Multi<Elemento> &m);
+
+// Implementación en un único procedimiento de las operaciones "siguienteElem",
+// "siguienteNumRep" y "avanza". Operacion siguienteElem: multi m -> natural
+// {Devuelve el siguiente elemento a visitar de m (según el orden establecido
+// por la operación menor). Parcial, la operación no está definida si no
+// existeSiguiente(m). } Operación siguienteNumRep: multi m -> natural
+//{ Devuelve la multiplicidad del siguiente elemento distinto a visitar de m
+//(según el orden establecido por
+// la operación menor). Parcial, la operación no está definida si no
+// existeSiguiente(m). } Operación avanza: multi m -> multi
+//{ Devuelve el multiconjunto resultante de avanzar el iterador de "m" al
+//siguiente elemento distinto a visitar (según el orden establecido por la
+// operación menor).} Parcial: la operación no está definida si no
+// existeSiguiente(m). }
 template <typename Elemento>
-void anyadir(Multi<Elemento>& m, const Elemento& e);
+void siguiente(Multi<Elemento> &m, Elemento &e, int &num);
 
-//
-template <typename Elemento>
-void quitar(Multi<Elemento>& m, const Elemento& e);
+// Sus valores multiconjuntos de elementos, es decir, colecciones de elementos
+// en las que puede haber ejemplares repetidos
+template <typename Elemento> struct Multi {
 
-//
-template <typename Elemento>
-int multiplicidad(Multi<Elemento>& m, const Elemento& e);
+  friend void vacio<Elemento>(Multi<Elemento> &m);
+  friend void anyadir<Elemento>(Multi<Elemento> &m, const Elemento &e);
+  friend void quitar<Elemento>(Multi<Elemento> &m, const Elemento &e);
+  friend int multiplicidad<Elemento>(Multi<Elemento> &m, const Elemento &e);
+  friend int cardinal<Elemento>(Multi<Elemento> &m);
+  friend void iniciarIterador<Elemento>(Multi<Elemento> &m);
+  friend bool existeSiguiente<Elemento>(Multi<Elemento> &m);
+  friend void siguiente<Elemento>(Multi<Elemento> &m, Elemento &e,
+                                  int &num); // preguntar si es mejor bool
 
-//
-template <typename Elemento>
-int cardinal(Multi<Elemento>& m );
-
-//
-template <typename Elemento>
-void iniciarIterador(Multi<Elemento>& m );
-
-//
-template <typename Elemento>
-bool existeSiguiente(Multi<Elemento>& m );
-
-//
-template <typename Elemento>
-void siguienteElem(Multi<Elemento>& m );
-
-//
-template <typename Elemento>
-void siguienteNumRep(Multi<Elemento>& m );
-
-//
-template <typename Elemento>
-void avanza(Multi<Elemento>& m );
-
-//
-template <typename Elemento>
-bool siguiente (Multi<Elemento>& m, Elemento& e, int& num );
-
-
-template <typename Elemento>
-struct Multi{
-
-    friend  void vacio<Elemento> (Multi<Elemento>& m );
-    friend  void anyadir<Elemento> (Multi<Elemento>& m, const Elemento& e  );
-    friend  void quitar<Elemento> (Multi<Elemento>& m, const Elemento& e  );
-    friend  int multiplicidad<Elemento> ( Multi<Elemento>& m, const Elemento& e );
-    friend  int cardinal<Elemento> (Multi<Elemento>& m  );
-    friend  void iniciarIterador<Elemento> ( Multi<Elemento>& m );
-    friend  bool existeSiguiente<Elemento> ( Multi<Elemento>& m );
-    friend  bool siguiente<Elemento> (Multi<Elemento>& m, Elemento& e, int& num );
-
-    private:
-        struct Celda{
-            Celda* siguiente;
-            Elemento clave;
-            int valor; //multiplicidad
-        };
-        int tamanyo;
-        Celda* primero;
-        Celda* iter;
+private:
+  struct Celda {
+    Celda *siguiente;
+    Elemento clave;
+    int valor; // multiplicidad
+  };
+  int tamanyo;
+  Celda *primero;
+  Celda *iter;
 };
 
-
+// Devuelve un multiconjunto vacío, sin elementos
+template <typename Elemento> void vacio(Multi<Elemento> &m) {
+  m.primero = nullptr;
+  m.tamanyo = 0;
+  m.iter = nullptr;
+}
+// Devuelve el multiconjunto igual al resultante de añadir un ejemplar del
+// elemento "e" a "m"
 template <typename Elemento>
-void vacio(Multi<Elemento>& m){
-    m.primero=nullptr;
-    m.tamanyo = 0;
-    m.iter = nullptr;
+void anyadir(Multi<Elemento> &m, const Elemento &e) {
+  if (m.primero == nullptr) { // vacia
+    m.primero = new typename Multi<Elemento>::Celda;
+    m.primero->clave = e;
+    m.primero->valor = 1;
+    m.primero->siguiente = nullptr;
+  } else { // no vacia
+    typename Multi<Elemento>::Celda *aux;
+    aux = nullptr;
+    if (e < m.primero->clave) { // nuevo menor que el primero
+      aux = m.primero;
+      m.primero = new typename Multi<Elemento>::Celda;
+      m.primero->clave = e;
+      m.primero->valor = 1;
+      m.primero->siguiente = aux;
+      m.tamanyo++;
+    } else {
+      if (e == m.primero->clave) {
+        m.primero->valor++;
+      } else { // buscar punto de insercción
+        aux = m.primero;
+        while (aux->siguiente != nullptr && (aux->siguiente)->clave < e) {
+          aux = aux->siguiente;
+        }
+        if (aux->siguiente != nullptr && e == (aux->siguiente)->clave) {
+          ((aux->siguiente)->valor)++;
+        } else {
+          typename Multi<Elemento>::Celda *nuevo;
+          nuevo = new typename Multi<Elemento>::Celda;
+          nuevo->clave = e;
+          (nuevo->valor) = 1;
+          nuevo->siguiente = aux->siguiente;
+          aux->siguiente = nuevo;
+          m.tamanyo++;
+        }
+      }
+    }
+  }
 }
 
+// Devuelve el multiconjunto igual al resultante de extraer un ejemplar del
+// elemento "e" a "m"; si no hay ningún elemento de "e" en "m", devuelve el
+// multiconjunto igual a m
 template <typename Elemento>
-void anyadir(Multi<Elemento>& m, const Elemento& e){
-    if(m.primero==nullptr){ //vacia
-        m.primero = new typename Multi<Elemento>::Celda;
-        m.primero->clave = e;
-        m.primero->valor = 1;
-        m.primero->siguiente=nullptr;
-    }else{ //no vacia 
-        typename Multi<Elemento>::Celda* aux;
-        aux = nullptr;
-        if(e<m.primero->clave){ //nuevo menor que el primero
-        aux=m.primero;
-        m.primero = new typename Multi<Elemento>::Celda;
-        m.primero->clave=e;
-        m.primero->valor=1;
-        m.primero->siguiente=aux;
-        m.tamanyo++;
-        }else{
-            if(e==m.primero->clave){
-                m.primero->valor++;
-            }else{ // buscar punto de insercción
-                aux=m.primero;
-                while(aux->siguiente!=nullptr && (aux->siguiente)->clave<e){
-                    aux = aux->siguiente;
-                }
-                if(aux->siguiente!=nullptr && e==(aux->siguiente)->clave){
-                    ((aux->siguiente)->valor)++;
-                }else{
-                    typename Multi<Elemento>::Celda* nuevo;
-                    nuevo = new typename Multi<Elemento>::Celda;
-                     nuevo->clave=e;
-                    (nuevo->valor)= 1;
-                    nuevo->siguiente= aux->siguiente;
-                    aux->siguiente = nuevo;
-                    m.tamanyo++;
-                 }
+void quitar(Multi<Elemento> &m, const Elemento &e) {
+  typename Multi<Elemento>::Celda *aux1;
+  typename Multi<Elemento>::Celda *aux2;
+  if (m.primero != nullptr) {      // no vacia
+    if (!(e < m.primero->clave)) { // mayor igual que el primer elemento
+      if (m.primero->clave == e) { // igual que el primer elemento
+        aux1 = m.primero;
+        m.primero = m.primero->siguiente;
+        delete aux1;
+        m.tamanyo--;
+      } else { // distinto que el primer elemento
+        bool parar = false;
+        aux1 = m.primero->siguiente;
+        aux2 = m.primero;
+        while (aux1 != nullptr && !parar) {
+          if (e < aux1->clave) {
+            parar = true;
+          } else if (e == aux1->clave) {
+
+            if (aux1->valor == 1) { // si valor == 1 elimina la celda
+              aux2->siguiente = aux1->siguiente;
+              delete (aux1);
+              m.tamanyo--;
+            } else { // sino decrementa valor
+              (aux1->valor)--;
             }
+            parar = true;
+          } else {
+            aux2 = aux1;
+            aux1 = aux1->siguiente;
+          }
         }
+      }
     }
-    
+  }
 }
 
-
+// Devuelve el número de ejemplares del elemento "e" en "m" (0 si no hay
+// ninguno)
 template <typename Elemento>
-void quitar(Multi<Elemento>& m, const Elemento& e){
-    typename Multi<Elemento>::Celda* aux1;
-    typename Multi<Elemento>::Celda* aux2;
-    if(m.primero!=nullptr){
-        if(!(e<m.primero->clave)){
-            if(m.primero->clave==e){
-                aux1=m.primero;
-                m.primero=m.primero->siguiente;
-                delete aux1;
-                m.tamanyo--;
-            }else{
-                bool parar=false;
-                aux1=m.primero->siguiente;
-                aux2=m.primero;
-                while(aux1 != nullptr && !parar){
-                    if(e<aux1->clave){
-                        parar=true;
-                    }else if(e==aux1->clave){
-                        aux2->siguiente=aux1->siguiente;
-                        delete(aux1);
-                        parar = true;
-                        m.tamanyo--;
-                    }else{
-                        aux2=aux1;
-                        aux1=aux1->siguiente;
-                    }
-                }
-            }
-        }
+int multiplicidad(Multi<Elemento> &m, const Elemento &e) {
+  typename Multi<Elemento>::Celda *aux;
+  aux = m.primero;
+  while (aux->siguiente != nullptr && aux->clave < e) {
+    aux = aux->siguiente;
+  }
+  if (aux != nullptr || aux->clave >= e) {
+    if (aux->clave == e) {
+      return (aux->valor);
     }
+  }
+  return 0;
 }
 
+// Devuelve el número total de elementos en "m", contando los repetidos
+template <typename Elemento> int cardinal(Multi<Elemento> &m) {
+  int cont = multiplicidad(m, m.primero->clave);
+  typename Multi<Elemento>::Celda *aux;
+  aux = m.primero;
+  while (aux->siguiente != nullptr) {
+    aux = aux->siguiente;
+    cont += multiplicidad(m, aux->clave);
+  }
 
+  return cont;
+}
+
+// Prepara el iterador para que el siguiente elemento a visitar sea el menor de
+// todo el multiconjunto "m" (según la operación menor del parámetro formal
+// elemento), si existe
+template <typename Elemento> void iniciarIterador(Multi<Elemento> &m) {
+  m.iter = m.primero;
+}
+
+// Devuelve falso si ya se han visitado todos los elementos de "m", devuelve
+// verdad en caso contrario
+template <typename Elemento> bool existeSiguiente(Multi<Elemento> &m) {
+  return m.iter != nullptr;
+}
+
+// Implementación en un único procedimiento de las operaciones "siguienteElem",
+// "siguienteNumRep" y "avanza". Operacion siguienteElem: multi m -> natural
+// {Devuelve el siguiente elemento a visitar de m (según el orden establecido
+// por la operación menor). Parcial, la operación no está definida si no
+// existeSiguiente(m). } Operación siguienteNumRep: multi m -> natural
+//{ Devuelve la multiplicidad del siguiente elemento distinto a visitar de m
+//(según el orden establecido por
+// la operación menor). Parcial, la operación no está definida si no
+// existeSiguiente(m). } Operación avanza: multi m -> multi
+//{ Devuelve el multiconjunto resultante de avanzar el iterador de "m" al
+//siguiente elemento distinto a visitar (según el orden establecido por la
+// operación menor).} Parcial: la operación no está definida si no
+// existeSiguiente(m). }
 template <typename Elemento>
-int multiplicidad(Multi<Elemento>& m, const Elemento& e){
-    typename Multi<Elemento>::Celda* aux;
-    aux=m.primero;
-    while(aux ->siguiente!= nullptr && aux->clave < e){
-        aux = aux->siguiente;
-    }
-    if(aux != nullptr || aux->clave >= e){
-        if(aux->clave == e){
-            return (aux->valor);
-        }
-    }
-    return 0;
+void siguiente(Multi<Elemento> &m, Elemento &e, int &num) {
+  if (existeSiguiente(m)) {
+    e = m.iter->clave;
+    num = m.iter->valor;
+    m.iter = m.iter->siguiente;
+  }
 }
-
-
-template <typename Elemento>
-int cardinal(Multi<Elemento>& m ){
-    return m.tamanyo;
-}
-
-
-template <typename Elemento>
-void iniciarIterador(Multi<Elemento>& m ){
-    m.iter = m.primero;
-}
-
-
-template <typename Elemento>
-bool existeSiguiente(Multi<Elemento>& m ){
-    return m.iter != nullptr;
-}
-
-
-template <typename Elemento>
-bool siguiente(Multi<Elemento>& m, Elemento& e, int& num){
-    if(existeSiguiente(m)){
-        e = m.iter->clave;
-        num =  m.iter->valor;
-        m.iter = m.iter->siguiente;
-        return true;
-    }else {
-        return false;
-    }
-
-}
-
-
-
-
 
 #endif
-
